e43fa76a093e27e50c42507e77ed8332511161e4
pcheruvu <pcheruvu@blackberry.com>
2017-11-18 23:44:36 -0800

    first commit

---
+++ src/components/Counter.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+        this.incrementIfOdd = this.incrementIfOdd.bind(this);
+        this.incrementAsync = this.incrementAsync.bind(this);
+    }
+
+    incrementIfOdd() {
+        if(this.props.value % 2 !== 0) {
+            this.props.onIncrement()
+        }
+    }
+
+    incrementAsync(){
+        setTimeout(this.props.onIncrement,1000)
+    }
+
+    render(){
+        const { value, onIncrement, onDecrement} = this.props
+        return(
+            <p>
+                Clicked: {value} times
+                {' '}
+                <button onClick={onIncrement}>
+                    +
+                </button>
+                {' '}
+                <button onClick={onDecrement}>
+                    - 
+                </button>
+                {' '}
+                <button onClick={this.incrementIfOdd}>
+                    Increment if Odd
+                </button>
+                {' '}
+                <button onClick={this.incrementAsync}>
+                    Increment async
+                </button>
+            </p>
+        )
+    }
+}
+
+Counter.prototypes = {
+    value: PropTypes.number.isRequired,
+    onIncrement: PropTypes.func.isRequired,
+    onDecrement: PropTypes.func.isRequired
+}
+
+export default Counter
